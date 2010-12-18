59fb7bf436751645088e2949d6cdf1cadec6df4b
Stephen Johnson <srj15@case.edu>
2010-12-18 00:30:21 -0500

    Minor tweaks to player control scheme

--- engine/player.py
+++ engine/player.py
@@ -20,10 +20,10 @@ class Player(actor.Actor):
             controls_map[settings.DOWN]: self.move_down,
         }
         self.release_controls = {
-            controls_map[settings.RIGHT]: self.zero_x,
-            controls_map[settings.LEFT]: self.zero_x,
-            controls_map[settings.UP]: self.zero_y,
-            controls_map[settings.DOWN]: self.zero_y,
+            controls_map[settings.RIGHT]: self.zero_x_right,
+            controls_map[settings.LEFT]: self.zero_x_left,
+            controls_map[settings.UP]: self.zero_y_up,
+            controls_map[settings.DOWN]: self.zero_y_down,
         }
     
     def on_key_press(self, symbol, modifiers):
@@ -38,11 +38,23 @@ class Player(actor.Actor):
             f()
             return pyglet.event.EVENT_HANDLED
     
-    def zero_x(self):
+    def zero_x_left(self):
+        if self.move_x == 1: return
         self.body.velocity[0] = 0
         self.move_x = 0
     
-    def zero_y(self):
+    def zero_x_right(self):
+        if self.move_x == -1: return
+        self.body.velocity[0] = 0
+        self.move_x = 0
+    
+    def zero_y_down(self):
+        if self.move_y == 1: return
+        self.body.velocity[1] = 0
+        self.move_y = 0
+    
+    def zero_y_up(self):
+        if self.move_y == -1: return
         self.body.velocity[1] = 0
         self.move_y = 0
     
