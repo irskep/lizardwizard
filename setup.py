3344f4334625fc7a6444952befa4e135b1768fc4
Stephen Johnson <srj15@case.edu>
2010-12-19 19:23:38 -0500

    Mac build

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+Usage (Mac):
+    python setup.py py2app
+"""
+
+import sys
+
+mainscript = 'LizardWizard.py'
+
+OPTIONS = dict(
+    argv_emulation=True,
+    frameworks=['pymunk/libchipmunk.dylib'],
+    plist = dict(CFBundleIconFile='lizardwizard.icns'), 
+    # PyRuntimeLocations=['/Library/Frameworks/Python.framework/Versions/2.7/Python']
+    #, '/System/Library/Frameworks/Python.framework/Versions/Current/Python'])
+)
+
+if sys.platform == 'darwin':
+     from setuptools import setup
+     extra_options = dict(
+         setup_requires=['py2app'],
+         app=[mainscript],
+         options={'py2app': OPTIONS},
+     )
+
+setup(
+    name='Lizard Wizard',
+    data_files=['engine','game', 'lizardwizard.icns'],
+                # 'noise', 'yaml', 'pyglet', 'pymunk'],
+    **extra_options
+)
